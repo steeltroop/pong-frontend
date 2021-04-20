182eb8962f3dd227f945fb436570c54ae465f841
onemo <wonmotime1@naver.ccom>
2021-04-20 14:00:29 +0900

    [ADD] score, countdown on ScoreBoard component

--- src/components/scoreBoard/ScoreBoard.js
+++ src/components/scoreBoard/ScoreBoard.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./ScoreBoard.module.css";
 
-const ScoreBoard = ({ count }) => {
+const ScoreBoard = (props) => {
+  const {
+    count,
+    isPlaying,
+    userScore,
+    partnerScore
+  } = props;
+
   return (
-    <div>
-      {count}
-    </div>
+    <>
+      {isPlaying
+        ? <div>{userScore} : {partnerScore}</div>
+        : <div>{count}</div>}
+    </>
   );
 };
 
