855e631bb1533719cfc7dda1376f2b7a8f284c57
yunsu <59559334+emune216@users.noreply.github.com>
2021-04-23 22:15:55 +0900

    [ADD] CSS

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,18 +2,23 @@ const ROUTES = {
   HOME: "/",
   LOGIN: "/auth/login",
   BATTLE: "/battle",
-  RANKING: "/ranking"
+  RANKING: "/ranking",
 };
 
 const NUMBERS = {
   INITIAL_COUNT: 3,
   END_COUNT: 0,
   DELAY: 1000,
-  SUBTRACT_NUMBER: 1
+  SUBTRACT_NUMBER: 1,
 };
 
 const URL = {
-  GOOGLE_LOGO: "https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+  GOOGLE_LOGO: "https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg",
 };
 
-export { URL, ROUTES, NUMBERS };
+const MESSAGE = {
+  FINDING_USER: "Finding User...",
+  MATCHING_USER: "Matching User!!",
+};
+
+export { URL, ROUTES, NUMBERS, MESSAGE };
