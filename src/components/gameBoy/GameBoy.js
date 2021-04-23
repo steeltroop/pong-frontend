8adaaebf924931613288ba699c277950f0a2379d
yunsu <than4502@gmail.com>
2021-04-23 11:19:34 +0900

    [ADD] CSS

--- src/components/gameBoy/GameBoy.js
+++ src/components/gameBoy/GameBoy.js
@@ -6,18 +6,11 @@ const GameBoy = (props) => {
   return (
     <div className={styles.gameBoyWrapper} >
       <div className={styles.gameBoyHead} >
-        <div className={styles.power} >
-          <p>
-            <span className={styles.ledButton} >⬤</span>
-            <span> ) ) )</span>
-          </p>
-          <p>POWER</p>
-        </div>
         <div className={styles.gameBoardWrapper} >
           {props.children}
         </div>
         <div className={styles.gameBoyMark} >
-          <h4>Game BOY <span>PONG</span></h4>
+          <h4>GameBOY <span>PONG!</span></h4>
         </div>
       </div>
     </div>
