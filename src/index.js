885bddc9900fbd1e16018677b8502b8a01e65bb2
yunsu <59559334+emune216@users.noreply.github.com>
2021-04-29 20:00:15 +0900

    [UPDATE] ENV naming

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import logger from "redux-logger";
+import { createLogger } from "redux-logger";
 import App from "./components/app/App";
 import reducer from "./redux/reducers";
 
-const store = createStore(reducer, applyMiddleware(logger));
+const middleware = [];
+
+if (process.env.NODE_ENV !== "production") {
+  const logger = createLogger();
+  middleware.push(logger);
+}
+
+const store = createStore(reducer, applyMiddleware(...middleware));
 
 ReactDOM.render(
   <Provider store={store}>
